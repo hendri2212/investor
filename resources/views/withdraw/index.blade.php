cf253edb21e84bc37af0b58fbcf7a95a5c0d980d
hendri2212 <cowok_cool320@yahoo.co.id>
2024-10-28 21:51:30 +0800

    change table to card

--- resources/views/withdraw/index.blade.php
+++ resources/views/withdraw/index.blade.php
@@ -33,27 +33,13 @@
             <td>{{ $additionalData[0]['business_days'] }} days</td>
         </tr>
     </table>
-    <p class="m-0 pt-4 fw-bold text-center text-uppercase text-primary">History Withdraw Transaction</p>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="text-center">No</th>
-                    <th>Date</th>
-                    <th>Nominal</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($withdrawls as $index => $withdraw)
-                <tr>
-                    <td class="text-center">{{ $loop->iteration }}</td>
-                    <td class="text-nowrap">{{ Carbon::parse($withdraw->date)->translatedFormat('d F Y H:i') }}</td>
-                    <td class="text-nowrap">Rp {{ number_format($withdraw->nominal, 0, ',', '.') }}</td>
-                    <td>{{ $withdraw->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <p class="m-0 pt-4 fw-bold h5 text-primary">History Withdraw Transaction</p>
+    @foreach ($withdrawls as $index => $withdraw)
+        <div class="card shadow mt-3">
+            <div class="card-body d-flex flex-column">
+                <span class="form-text">{{ Carbon::parse($withdraw->date)->translatedFormat('d F Y H:i') }}</span>
+                <span class="fw-bold text-success">Rp {{ number_format($withdraw->nominal, 0, ',', '.') }}</span>
+            </div>
+        </div>
+    @endforeach
 @endsection
